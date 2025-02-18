1bb37f06b56682ccf46372d67cb7b51aadc11d83
Tom MacWright <tom@macwright.com>
2025-02-18 09:07:42 -0500

    Add test for new error state

--- test/bad.test.ts
+++ test/bad.test.ts
@@ -1,102 +1,108 @@
-import { describe, expect, it } from 'vitest';
-import { check, getIssues, HintError } from '../lib';
-import * as Path from 'path';
-import { readFileSync, readdirSync } from 'fs';
+import { describe, expect, it } from "vitest";
+import { check, getIssues, HintError } from "../lib";
+import * as Path from "path";
+import { readFileSync, readdirSync } from "fs";
 
-describe('check', () => {
-  it('invalid root object', () => {
-    expect(() => check(JSON.stringify([]))).toThrow(HintError);
-    expect(() => check(JSON.stringify({}))).toThrow(HintError);
-    try {
-      check('{"foo": 1.}');
-    } catch (e) {
-      expect(e.issues).toHaveLength(1);
-    }
-    expect(() => check(JSON.stringify({ type: ['foo'] }))).toThrow(HintError);
-    expect(() => check(JSON.stringify({ type: 'foo' }))).toThrow(HintError);
-    expect(getIssues(JSON.stringify({}))).toEqual([
-      {
-        from: 0,
-        message: 'This GeoJSON object is missing its type member.',
-        severity: 'error',
-        to: 2,
-      },
-    ]);
-    expect(getIssues(JSON.stringify({ type: 'test' }))).toEqual([
-      {
-        from: 0,
-        message: 'This type of GeoJSON object is not allowed here.',
-        severity: 'error',
-        to: 15,
-      },
-    ]);
-  });
+describe("check", () => {
+	it("invalid input", () => {
+		// @ts-expect-error this is a test for wrong usage
+		expect(() => check({})).toThrow(TypeError);
+		// @ts-expect-error this is a test for wrong usage
+		expect(() => check([])).toThrow(TypeError);
+	});
+	it("invalid root object", () => {
+		expect(() => check(JSON.stringify([]))).toThrow(HintError);
+		expect(() => check(JSON.stringify({}))).toThrow(HintError);
+		try {
+			check('{"foo": 1.}');
+		} catch (e) {
+			expect(e.issues).toHaveLength(1);
+		}
+		expect(() => check(JSON.stringify({ type: ["foo"] }))).toThrow(HintError);
+		expect(() => check(JSON.stringify({ type: "foo" }))).toThrow(HintError);
+		expect(getIssues(JSON.stringify({}))).toEqual([
+			{
+				from: 0,
+				message: "This GeoJSON object is missing its type member.",
+				severity: "error",
+				to: 2,
+			},
+		]);
+		expect(getIssues(JSON.stringify({ type: "test" }))).toEqual([
+			{
+				from: 0,
+				message: "This type of GeoJSON object is not allowed here.",
+				severity: "error",
+				to: 15,
+			},
+		]);
+	});
 
-  it('invalid coordinates', () => {
-    expect(() => check(JSON.stringify({ type: 'Point' }))).toThrow(HintError);
-    expect(() =>
-      check(JSON.stringify({ type: 'Point', coordinates: null }))
-    ).toThrow(HintError);
-  });
+	it("invalid coordinates", () => {
+		expect(() => check(JSON.stringify({ type: "Point" }))).toThrow(HintError);
+		expect(() =>
+			check(JSON.stringify({ type: "Point", coordinates: null })),
+		).toThrow(HintError);
+	});
 
-  it('bad points', () => {
-    expect(() =>
-      check(JSON.stringify({ type: 'Point', coordinates: [1] }))
-    ).toThrow(HintError);
-    expect(() =>
-      check(JSON.stringify({ type: 'Point', coordinates: [1, 2, 3, 4] }))
-    ).toThrow(HintError);
-  });
+	it("bad points", () => {
+		expect(() =>
+			check(JSON.stringify({ type: "Point", coordinates: [1] })),
+		).toThrow(HintError);
+		expect(() =>
+			check(JSON.stringify({ type: "Point", coordinates: [1, 2, 3, 4] })),
+		).toThrow(HintError);
+	});
 
-  it('bad multipoints', () => {
-    expect(() =>
-      check(JSON.stringify({ type: 'MultiPoint', coordinates: [[true]] }))
-    ).toThrow(HintError);
-  });
+	it("bad multipoints", () => {
+		expect(() =>
+			check(JSON.stringify({ type: "MultiPoint", coordinates: [[true]] })),
+		).toThrow(HintError);
+	});
 
-  it('geometry collection with null', () => {
-    expect(() =>
-      check(JSON.stringify({ type: 'GeometryCollection', geometries: [null] }))
-    ).toThrow(HintError);
-  });
+	it("geometry collection with null", () => {
+		expect(() =>
+			check(JSON.stringify({ type: "GeometryCollection", geometries: [null] })),
+		).toThrow(HintError);
+	});
 
-  it('forbidden properties', () => {
-    expect(() =>
-      check(
-        JSON.stringify({ type: 'Point', coordinates: [1, 2], properties: {} })
-      )
-    ).toThrow(HintError);
-  });
+	it("forbidden properties", () => {
+		expect(() =>
+			check(
+				JSON.stringify({ type: "Point", coordinates: [1, 2], properties: {} }),
+			),
+		).toThrow(HintError);
+	});
 
-  it('bad json', () => {
-    try {
-      check(
-        `{
+	it("bad json", () => {
+		try {
+			check(
+				`{
     "type": "MultiPoint"
     "coordinates": [["foo", "bar"]]
-}`
-      );
-    } catch (e) {
-      expect(e.issues[0]).toEqual({
-        message:
-          'Invalid JSON: Unexpected token String("coordinates") found. (3:5)',
-        from: 0,
-        to: 0,
-        severity: 'error',
-      });
-    }
-  });
+}`,
+			);
+		} catch (e) {
+			expect(e.issues[0]).toEqual({
+				message:
+					'Invalid JSON: Unexpected token String("coordinates") found. (3:5)',
+				from: 0,
+				to: 0,
+				severity: "error",
+			});
+		}
+	});
 
-  describe('works with fixtures', () => {
-    const fixtureNames = readdirSync(Path.join(__dirname, './fixture/bad/'));
-    for (const name of fixtureNames) {
-      it(`fixture: ${name}`, () => {
-        const input = readFileSync(
-          Path.join(__dirname, './fixture/bad/', name),
-          'utf8'
-        );
-        expect(() => check(input)).toThrow(HintError);
-      });
-    }
-  });
+	describe("works with fixtures", () => {
+		const fixtureNames = readdirSync(Path.join(__dirname, "./fixture/bad/"));
+		for (const name of fixtureNames) {
+			it(`fixture: ${name}`, () => {
+				const input = readFileSync(
+					Path.join(__dirname, "./fixture/bad/", name),
+					"utf8",
+				);
+				expect(() => check(input)).toThrow(HintError);
+			});
+		}
+	});
 });
